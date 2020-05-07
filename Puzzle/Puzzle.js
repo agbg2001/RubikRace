582cbefde1d561581365798a20c56eea9091150e
Angela <a28he@uwaterloo.ca>
2020-05-07 12:15:26 -0400

    Folder files hold necessary info about the game

--- Puzzle/Puzzle.js
+++ Puzzle/Puzzle.js
@@ -1,2 +1,9 @@
 //Code for the slider puzzle
 //Stephen will start on this?
+
+
+// 5 by 5 grid
+// 4 of each colour (we should probably designate a value for every colour or smth)
+// one open slot for movement 
+// center 3 by 3 needs to check against the goal
+// colours : white, blue, red, orange, yellow, green
